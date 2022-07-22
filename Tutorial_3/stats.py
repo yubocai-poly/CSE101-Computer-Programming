e8ddf48e0faf0c0cfdb7c834f8d2163909dff43c
Yubo Cai <98619371+yubocai-poly@users.noreply.github.com>
2022-07-22 02:33:45 +0200

    Add files via upload

--- Tutorial_3/stats.py
+++ Tutorial_3/stats.py
@@ -1,128 +1,145 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct  6 23:54:19 2021
+CSE101 - Computer Programming 
+Tutorial 3 - CSE101 Statistics
+Created on Wed Sep 29 21:01:01 2021
 
-@author: apple
+@author: Yubo Cai
 """
 
-from numpy import *
 
-# Exercise 1
+# Exercise 1 - Computing Averages
 def average(numlist):
-    """Return average of list of numbers"""
-    ave = mean(numlist)
-    return round(ave,2)
+    return round(sum(numlist) / len(numlist), 2)
 
-# Exercise 2
+
+# Exercise 2 - Convert a string of floats to a list
 def string_floats_to_list(string_floats):
     """Return a list from the string of floats string_floats."""
-    sep = string_floats.split()
-    a_float = []
-    for num in sep:
-        a_float.append(float(num))
-    return a_float
+    lis = string_floats.split(' ')
+    return lis
 
-# Exercise 3
+
+# Exercise 3 - Student Data
 def student_data(data_string):
     """Compute (name, results) tuple from the string data_string."""
-    data = data_string.split()
-    login_name = data[0]
-    result = []
-    for i in range(1,len(data)):
-        result.append(float(data[i]))
-    return (login_name, result)
+    name, num = data_string.split(' ')[0], data_string.split(' ')[1:]
+    for i in range(len(num)):
+        num[i] = float(num[i])
 
-# Exercise 4
+    return (name, num)
+
+
+# Exercise 4 - Convert a tuple to a string
 def tuple_to_string(name, results):
     """Return string from (name, results) tuple"""
-    name_input = name
-    sep_result = ' '.join([str(x) for x in results])
-    return (name_input + ' ' + sep_result)
+    for i in range(len(results)):
+        results[i] = str(results[i])
 
-# Exercise 5
+    return name + ' ' + ' '.join(results)
+
+
+# Exercise 5 - Reading Student Data
 def read_student_data(filename):
     """Return list of student data from file"""
-    list_1 = []
-    with open(filename,'r') as file:
-        for line in file:
-            list_1.append(student_data(line))
-    return list_1
+    with open(filename, 'r') as f:
+        return [student_data(line.strip()) for line in f]
 
-# Exercise 6
+
+# Exercise 6 - Extracting Averages
 def extract_averages(filename):
     """Return list of name and average for each line in file"""
-    a = read_student_data(filename)
-    s = []
-    for t in a:
-        s.append((t[0],average(t[1])))
-    return s
+    data = read_student_data(filename)
+    result = []
 
-# Exercise 7
+    for i in range(len(data)):
+        ave = 0
+        for j in range(len(data[i][1])):
+            ave += float(data[i][1][j])
+        ave = round(ave / len(data[i][1]), 2)
+        result.append((data[i][0], ave))
+
+    return result
+
+
+# Exercise 7 - Filtering Lists of Scores
 def discard_scores(numlist):
     """Filter numlist: construct a new list from numlist with
     the first two, and then the lowest two, scores discarded.
     """
-    numl = [num for num in numlist]
-    for i in range(2):
-        numl.remove(numl[0])
+    lis = numlist[2:]
+    for _ in range(2):
+        lis.remove(min(lis))
 
-    for i in range(2):
-        mini = numl[0]
-        for i in range(len(numl)):
-            if mini > numl[i]:
-                mini = numl[i]
-                i = i + 1
-        numl.remove(mini)
-        
-    return numl
+    return lis
 
+
+# Exercise 8 - Summaries per Student
 def summary_per_student(infilename, outfilename):
     """Create summaries per student from the input file 
     and write the summaries to the output file.
     """
-    # I receive the hint from Junyuan Wang
-    
-    list_out = []
-    whole_sum_student = 0
     data = read_student_data(infilename)
-    for student in data:
-        name_part = tuple_to_string(student[0], discard_scores(student[1]))
-        sum_student = 0
-        for num in discard_scores(student[1]):
-            sum_student += num
-        whole_sum_student += sum_student
-        number_part = 'sum: ' + str(round(sum_student,2))
-        data_person = name_part + ' ' + number_part
-        list_out.append(data_person)
-        
-    total_ave = round(whole_sum_student/len(list_out),2)
-    str_total = 'total average: ' + str(total_ave)
-    list_out.append(str_total)
-    
-    with open(outfilename, 'w') as sample:
-        for line in list_out:
-            sample.write(line + '\n')
-            
+    total_num = 0
+    with open(outfilename, 'w') as f:
+        for i in range(len(data)):
+            sum_num = round(sum(discard_scores(data[i][1])), 2)
+            total_num += sum_num
+            lis = discard_scores(data[i][1])
+            f.write(
+                tuple_to_string(data[i][0], lis) + ' sum: ' + str(sum_num) +
+                '\n')
+
+        f.write('total average: ' + str(round(total_num / len(data), 2)))
+
+
+# Exercise 9 - Summaries per Tutorial
 def summary_per_tutorial(infilename, outfilename):
     """Create summaries per student from infile and write to outfile."""
-    
     data = read_student_data(infilename)
-    l = []
-    for i in range(len(data[0][1])):
-        column = []
-        
-        for j in range(len(data)):
-            td_score = data[j][1][i]
-            column.append(td_score)
-            
-        ave_td = average(column)
-        max_td = max(column)
-        min_td = min(column)
-       
-        l.append('TD' + str(i+1) +': average: ' + str(ave_td) + ' min: ' + str(min_td) + ' max: ' + str(max_td))
-    
-    with open(outfilename, 'w') as sample_2:
-        for line in l:
-            sample_2.write(line + '\n')
-        
+    with open(outfilename, 'w') as f:
+        for i in range(len(data[0][1])):
+            lis = []
+            for j in range(len(data)):
+                lis.append(data[j][1][i])
+            ave_num = average(numlist=lis)
+            min_num = min(lis)
+            max_num = max(lis)
+            f.write(
+                f'TD {i + 1}: average: {ave_num}, min: {min_num}, max: {max_num}\n'
+            )
+
+
+# Exercise 10 - Sending Results to Students
+def generate_emails(filename):
+    """Generate emails to students with their results"""
+    data = read_student_data(filename)
+    for i in range(len(data)):
+        with open(data[i][0] + '.txt', 'w') as f:
+            f.write(f'To: {data[i][0]}@polytechnique.edu\n\n')
+            f.write(
+                'This is to notify you of your final results for the CSE101 course, see\ntable below.  (Note that the two first and two lowest scores are\nexcluded from the result.)\n\n'
+            )
+
+            string = ''
+            for j in range(len(data[i][1])):
+                string += f'TD{j+1}  '
+            string += 'Result\n'
+            f.write(string)
+
+            string2 = ''
+            for j in range(len(data[i][1])):
+                string2 += f'-----'
+            string2 += '------\n'
+            f.write(string2)
+
+            string3 = ''
+            for j in range(len(data[i][1])):
+                string3 += f'{data[i][1][j]}  '
+            string3 += str(round(sum(discard_scores(data[i][1])), 2))
+            f.write(string3 + '\n\n')
+
+            f.write('Best regards,\n')
+            f.write('and please get back to me if you have any questions,\n')
+            f.write('Your Teacher')
