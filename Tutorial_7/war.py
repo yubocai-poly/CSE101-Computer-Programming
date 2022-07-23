9e70edfd73267a39823fa57dfa0fa51bac4b3472
Yubo Cai <98619371+yubocai-poly@users.noreply.github.com>
2022-07-23 23:04:24 +0200

    Add files via upload

--- Tutorial_7/war.py
+++ Tutorial_7/war.py
@@ -1,13 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+CSE101 - Computer Programming 
+Tutorial 7 - Naive War (card game)
+Created on Wed Sep 29 21:01:01 2021
+
+@author: Yubo Cai
+"""
+
 import random
 
 
+# Exercise 1 - A Class for Playing Cards
 class Card:
-    """French playing cards.
+    """
+    French playing cards.
 
     Class attributes:
     suit_names -- the four suits Clubs, Diamonds, Hearts, Spades
     rank_names -- the 13 ranks in each suit: Two--Ten, Jack, Queen, King, Ace
+
+    Data attributes:
+    suit, rank -- the Card's suit and rank, as indices into the lists above
     """
+
     suit_names = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
     rank_names = [
         'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten',
@@ -15,46 +31,55 @@ class Card:
     ]
 
     def __init__(self, suit, rank):
-        self.suit = suit
-        self.rank = rank
+        self.suit = suit  # from 0 to 3
+        self.rank = rank  # from 0 to 12
 
     def __str__(self):
-        return (self.rank_names[self.rank] + ' of ' +
-                self.suit_names[self.suit])
+        """
+        Return a string representation of the card.
+        """
+        return f'{self.rank_names[self.rank]} of {self.suit_names[self.suit]}'
+
+# Interlude: Some Cards Are More Equal Than Others
 
     def __eq__(self, other):
-        return self.rank == other.rank and self.suit == other.suit
+        """
+        Return True if self and other have the same rank and suit.
+        """
+        return self.suit == other.suit and self.rank == other.rank
 
     def __gt__(self, other):
         # returns True if and only if this Card is higher than the other
         if self.rank > other.rank:
             return True
-        elif self.rank < other.rank:
-            return False
+        elif self.rank == other.rank:
+            return self.suit > other.suit
         else:
-            if self.suit > other.suit:
-                return True
-            else:
-                return False
+            return False
 
 
+# Exercise 2 - A Class for Decks of Cards
 class Deck:
-    """A deck of Cards.
+    """
+    A deck of Cards.
 
     Data attributes:
     cards -- a list of all Cards in the Deck
     """
+
     def __init__(self, minrank):
         self.cards = []
-        for i in range(4):
-            for j in range(minrank, 13):
-                self.cards.append(Card(i, j))
+        for suit in range(4):
+            for rank in range(minrank, 13):
+                self.cards.append(Card(suit, rank))
 
     def __str__(self):
-        str_part = ''
-        for element in self.cards:
-            str_part += str(element) + ', '
-        return str_part[:-2]
+        """
+        Return a string representation of the Deck.
+        """
+        return ', '.join(str(card) for card in self.cards)
+
+# Exercise 3 - More about Decks
 
     def pop(self):
         """Remove and return last card from deck."""
@@ -64,28 +89,36 @@ class Deck:
 
     def shuffle(self):
         """Shuffle the deck."""
-        random.shuffle(self.cards)
+        return random.shuffle(self.cards)
 
 
+# Exercise 4 - A Class for Players
 class Player:
-    """A player of the card game.
+    """
+    A player of the card game.
 
     Data attributes:
     name -- the name of the player
     cards -- a list of all the player's cards (their "hand")
     """
+
     def __init__(self, name):
         self.name = name
         self.cards = []
 
     def __str__(self):
+        """
+        Return a string representation of the Player.
+        """
         if self.cards == []:
             return f'Player {self.name} has no cards'
         else:
-            card = ''
-            for car in self.cards:
-                card += f'{str(car)}, '
-        return f'Player {self.name} has: ' + card[:-2]
+            cards = ''
+            for card in self.cards:
+                cards += f'{str(card)}, '
+            return f'Player {self.name} has: {cards[:-2]}'
+
+# Exercise 5 - Players Playing
 
     def add_card(self, card):
         """Add card to this player's hand."""
@@ -93,75 +126,109 @@ class Player:
 
     def num_cards(self):
         """Return the number of cards in this player's hand."""
-        num = len(self.cards)
-        return num
+        return len(self.cards)
 
     def remove_card(self):
         """Remove the first card from this player's hand and return it."""
-        c = self.cards[0]
-        self.cards.remove(self.cards[0])
-        return c
+        return self.cards.pop(0)
 
 
+# Exercise 6 - A Class for Card Games
 class CardGame:
-    """A class for playing card games.
+    """
+    A class for playing card games.
+
     Data attributes:
     players -- a list of Player objects which participate in the game
     deck -- a Deck of Cards used for playing
     numcards -- the number of Cards in the game
     """
-    def __init__(self, player_names, minrank):
-        self.players = []
-        for i in range(len(player_names)):
-            pl = Player(player_names[i])
-            self.players.append(pl)
 
+    def __init__(self, player_names, minrank):
         self.deck = Deck(minrank)
         self.numcards = len(self.deck.cards)
+        self.players = []
+        for name in player_names:
+            self.players.append(Player(name))
 
     def __str__(self):
-        st = ''
-        for player in self.players:
-            st += str(player)
-            st += '\n'
-        return st[:-1]
+        """
+        Return a string representation of the CardGame.
+        """
+        string = ''
+        for name in self.players:
+            string += f'{name}\n'
+        return string[:-1]
+
+# Exercise 7 - Dealing Cards
 
     def shuffle_deck(self):
         """Shuffle this game's deck."""
         self.deck.shuffle()
 
     def deal_cards(self):
-        """Deal all of the cards in the deck to the players, round-robin."""
+        """
+        这道题用双指针的方法解决
+        Deal all of the cards in the deck to the players, round-robin.
+        """
         n = 0
-        for i in range(len(self.deck.cards)):
+        for i in range(self.numcards):
             self.players[n].add_card(self.deck.pop())
             n += 1
             if n == len(self.players):
                 n = 0
-        # This is the hint from Junyuan Wang
-    
+
+
+# Exercise 8 - Playing “Find the highest”
+
     def simple_turn(self):
-        """Play a very simple game.
+        """
+        Play a very simple game.
         For each player, play the first card.
         The winner is the player with the highest cards.
         """
         st = ''
-        card_record = []
+        record = []
         st_card = self.players[0].cards[0]
-        max_player = self.players[0]
+        winner = self.players[0]
 
         for i in range(len(self.players)):
-            if self.players[i].cards[0]>st_card:
-                max_player = self.players[i]
-                st_card = self.players[i].cards[0]
-            card_record.append(self.players[i].cards[0])
-            st += 'Player ' + str(self.players[i].name) + ': ' + str(self.players[i].cards[0]) + '\n'
-            
+            if self.players[i].cards:
+                if self.players[i].cards[0] > st_card:
+                    st_card = self.players[i].cards[0]
+                    winner = self.players[i]
+                record.append(self.players[i].cards[0])
+                st += f'Player {str(self.players[i].name)}: {str(self.players[i].cards[0])}\n'
+
         print(st)
-        return (max_player.name, card_record)
+        return winner.name, record 
+
+
+# Exercise 9 - Playing this simple game
 
     def play_simple(self):
-        return 'Grace'
-    """ Sorry this question is too difficult for me and I really
-    tried so many times still not working, so I saw if return directly 
-    can have the reight result so I just use it directly"""
+        """
+        这个游戏存在一些逻辑硬伤, 在发牌阶段，会从牌堆的最后一张牌开始发牌, 并且牌堆的顺序是完全有序的, 因此第一个得到牌的玩家的点数一定比其他人大,
+        所以最后的赢家也相对应一定是第一个玩家, 所以最后甚至可以直接返回Grace
+        """
+        num = self.numcards // len(self.players)
+        for i in range(num):
+            winner, record = self.simple_turn()
+            for player in self.players:
+                if player.name != winner and player.cards:
+                    player.remove_card()
+                elif player.name == winner:
+                    player.remove_card()
+                    for card in record:
+                        player.add_card(card)
+            winner = ''
+            record = []
+        
+        for player in self.players:
+            if player.num_cards() == self.numcards:
+                return player.name
+
+        
+        
+        
+        
